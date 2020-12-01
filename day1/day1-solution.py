d98037978e8a416982baf19e0a5f99a2e3551bba
Ursa Kacar <kacar.ursa@gmail.com>
2020-12-01 21:35:25 +0100

    AOC day 1 solution

---
+++ day1/day1-solution.py
@@ -0,0 +1,27 @@
+# DAY 1: Advent Of Code 2020
+
+inputFile = open('day1-input.txt', 'r')
+numbers = inputFile.readlines()
+
+
+# Finds two entries in day1-input.txt that sum to 2020 and multiplies them
+def twoNumbers():
+    for x in numbers:
+        for y in numbers:
+            if int(x) + int(y) == 2020:
+                result = int(x) * int(y)
+                print('2020 = ', x, ' + ', y, ', result of multiplication: ', result)
+                return
+
+# Finds three entries in day1-input.txt that sum to 2020 and multiplies them
+def threeNumbers():
+    for a in numbers:
+        for b in numbers:
+            for c in numbers:
+                if int(a) + int(b) + int(c) == 2020:
+                    result = int(a) * int(b) * int(c)
+                    print('2020 = ', a, ' + ', b, ' + ', c, ', result of multiplication: ', result)
+                    return
+
+twoNumbers()
+threeNumbers()
