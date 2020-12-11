eae4a9ee3e97fd1523a4ffcf283da3168a75d72f
Ursa Kacar <kacar.ursa@gmail.com>
2020-12-11 21:31:59 +0100

    rename folders

---
+++ 11-day/day11-solution.py
@@ -0,0 +1,15 @@
+# DAY X: Advent Of Code 2020
+
+inputFile = open('day8-example-input.txt', 'r')
+data = inputFile.readlines()
+
+# PART 1: 
+
+def part1():
+    
+# PART 2: 
+
+def part2():
+   
+part1()
+part2()
